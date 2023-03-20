fef9c61b1331ed82e0148828510a3e01354d65fa
wvovaw <vladimir5313375015@gmail.com>
2023-03-20 08:48:43 +0500

    Remove obsolete code, add scss, Nice navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
 import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), vue()]
+  integrations: [vue(), image(), tailwind()]
 });
