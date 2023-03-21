56ce61e36ec6241e41693453de96b9f56da03571
wvovaw <vladimir5313375015@gmail.com>
2023-03-22 01:09:12 +0500

    Remove tailwind, add tailwinds preflight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), image(), tailwind()]
+  markdown: {
+    shikiConfig: {
+      theme: "vitesse-light"
+    }
+  },
+  integrations: [vue(), image()]
 });
