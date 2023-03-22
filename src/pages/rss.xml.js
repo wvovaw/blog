0a0c45b9ec49e47964258c431db9f59b96a4944d
wvovaw <vladimir5313375015@gmail.com>
2023-03-22 17:41:22 +0500

    remove obsolete packages

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,14 @@
 import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function get() {
+  console.log("Base url now: ", import.meta.env.PUBLIC_BASE_URL);
   return rss({
     stylesheet: "public/rss/styles.xsl",
     title: "Wvovaw blog",
     description:
       "I post here my knowledges and experience that people might consider as useful",
     site: import.meta.env.PUBLIC_BASE_URL,
+    // site: "https://wvovaw.github.io/blog",
     items: await pagesGlobToRssItems(import.meta.glob("./blog/**/*.md")),
     customData: `<language>en-us</language>`,
   });
