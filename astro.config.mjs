15262e43c9643a6c1d172d76b19fe88e5779db5b
wvovaw <vladimir5313375015@gmail.com>
2023-03-22 17:23:42 +0500

    Add github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://wvovaw.github.io",
-  base: "/b/",
+  base: "/blog",
   markdown: {
     shikiConfig: {
       theme: "vitesse-dark",
