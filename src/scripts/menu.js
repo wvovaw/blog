e22e216b1b99579c2516dcd06de930ef57fd43c7
wvovaw <vladimir5313375015@gmail.com>
2023-03-24 15:37:43 +0500

    Adapted navbar for mobile with nice hamburger menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,12 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
+function toggleMenu() {
   document.querySelector('.nav').classList.toggle('expanded');
-});
+  document.querySelector('.hamburger').classList.toggle('hidden');
+  document.querySelector('.cross').classList.toggle('hidden');
+  document.querySelector('html').classList.toggle('prevent-scroll');
+}
+
+// open menu button
+document.querySelector('.hamburger').addEventListener('click', toggleMenu);
+
+// close menu button
+document.querySelector('.cross').addEventListener('click', toggleMenu);
